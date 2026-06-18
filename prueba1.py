259517112b3306fbaf8d61fe5412540a7ef60e75
Voliber1 <ol.egly@duocuc.cl>
2026-06-18 15:44:31 -0400

    terminado el prueba1.py

--- prueba1.py
+++ prueba1.py
@@ -1,3 +1,9 @@
+def buscar_mascota (lista_m, nombre_m):
+    for i in range (len(lista_m)):
+        if lista_m[i]["nombre"] == nombre_m:
+            return i #retorna la posicion
+    return -1 #se termina el ciclo porque no se encontro nada
+
 def mostar_menu ():
     print("===============================")
     print("1.- Agregar mascota")
@@ -7,6 +13,7 @@ def mostar_menu ():
     print("5.- Mostrar mascota")
     print("6.- Salir")
     print("===============================")
+
 def opciones ():
     while True:
         try:
@@ -19,31 +26,32 @@ def opciones ():
                 
         except ValueError:
             print("Error solo ingrese numeros del 1 al 6")
-
+#datos de lista de mascota
 def agregar_mascota (lista_m):
     nombre=input("Ingrese el nombre de su mascota   ")
-    correcta = agregar_mascota(nombre)
+    correcta = validacion_nombre(nombre)
     if not correcta:
         print("el nombre no puede estar en blanco")
         return
-    especie=input("Ingrese la especie de su mascota     ").lower()
-    correcta =agregar_mascota(especie)
+    especie=input("Ingrese la especie de su mascota     ")
+    correcta =validacion_especie(especie)
     if not correcta:
         print("solo atendemos perro, gato y ave")
         return
     edad=input("Ingrese la edad de la mascota (solo años)   ")
-    correcta = agregar_mascota(edad)
+    correcta = validacion_edad(edad)
     if not correcta:
         print("la edad debe ser un numero entero mayor a cero")
         return
     mascota = {
         "nombre": nombre.strip(),
-        "especie": especie.strip().lower,
+        "especie": especie.strip().lower(),
         "edad":int(edad),
         "vacunada":False
     }
     lista_m.append(mascota)
     print("Mascota agregada correctamente")
+#validacion de datos de lista de mascota
 def validacion_nombre (name):
     return name.strip() != ""
 
@@ -54,19 +62,64 @@ def validacion_especie (especie):
 def validacion_edad (edad):
     #isdigit revisa si un string contiene solo digitos osea es un numero 
     return edad.isdigit() and int(edad) > 0
+
+def actualizar_vacunas(lista_m):
+    for m in lista_m:
+        if m["edad"] >= 1:
+            m["vacunada"]=True
+        else:
+            m["vacunada"]=False
+
 datos_mascota=[]
 op = 0
 while op !=6:
     mostar_menu()
     op = opciones()
 
-    if op ==1:
+    if op == 1:
         agregar_mascota(datos_mascota)
-    elif op ==2:
-        print()
+    elif op == 2:
+        print("**** Buscar Mascota ****")
+        nom=input("ingrese el nombre de la mascota para buscar: ")
+        posicion =buscar_mascota(datos_mascota, nom)
+        if posicion != -1:
+            m = datos_mascota[posicion]
+            print(f"mascota encontrada en la posicion: {posicion}")
+            print(f"nombre mascota: {m["nombre"]}")
+            print(f"especie mascota: {m["especie"]}")
+            print(f"edad mascota: {m["edad"]}")
+            print(f"vacunada: {m["vacunada"]}")
+        else: 
+            print(f"no se encontro la mascora con el nombre: {nom}")
     elif op ==3:
-        print()
+        print("**** Eliminar Mascota ****")
+        nom =input("ingrese el nombre de la mascota a eliminar: ")
+
+        if posicion != -1:
+            datos_mascota.pop(posicion)
+            print("mascota eliminada correctamente")
+        else:
+            print("la mascota con este nombre no se encuentra registrada")
+
     elif op ==4:
-        print()
+        #para llamar lista que no retornan datos
+        actualizar_vacunas(datos_mascota)
+        print("estado actualizado")
+
     elif op ==5:
-        print()
+        #llamamos a la funcion de las vacunas
+        actualizar_vacunas(datos_mascota)
+        #mostar sus datos 
+        if len(datos_mascota)==0:
+            print("no hay mascotas en la lista")
+        else:
+            print("== lista de mascotas ==")
+            for m in datos_mascota:
+                print(f"nombre   : {m["nombre"]}")
+                print(f"especie  : {m["especie"]}")
+                print(f"edad     : {m["edad"]}")
+                #varialble para poder cambiar e valor 
+                estado ="AL DIA" if m["vacunada"] else "pendiente"
+                print(f"estado vacuna: {estado}")
+    elif op == 6:
+        print("gracias por usar el sistema")
